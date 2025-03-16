44105f746779771facda8301bb805975fc0c194f
Max Väistö <max.vaisto@gmail.com>
2025-03-16 19:18:49 +0200

    Updated vite config to work with intellij Browser live edit

--- part5/bloglist-frontend/vite.config.js
+++ part5/bloglist-frontend/vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
+    host: '127.0.0.1',
+    port: 5173,
+     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:3003',
         changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
-    }
+    },
   },
-})
+});
